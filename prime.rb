069c1f7fb5ac18abcaacf8817c756385f55f79d3
Chaim Bryski <luzybry94@gmail.com>
2020-08-07 23:01:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
 # Add  code here!
+# def prime?(integer)
+#   if integer <= 1
+#     false
+# --> prime number must be greater than 1
+#   else
+#     (2..integer-1).to_a.each do |num|
+
+#     if integer % num == 0
+#       return false
+#     end
+#     end
+#     true
+#   end
+# end
+
+def prime?(integer) 
+  if integer <= 1 
+    false 
+  else
+    (2..integer-1).to_a.all? do |num|
+      integer % num != 0
+    end
+  end
+end
