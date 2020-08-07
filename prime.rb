bf56ef39a572538a3df32cffe9f67892ca3edb6f
Chaim Bryski <luzybry94@gmail.com>
2020-08-07 23:07:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+# def prime?(integer)
+#   if integer <= 1
+#     false
+# --> prime number must be greater than 1
+#   else
+#     (2..integer-1).to_a.each do |num|
+# --> create array of numbers from 2 to the number entered as an argument ('integer') and iterate through that array
+#     if integer % num == 0
+#       return false
+# --> if integer is evenly divisible, return false (not a prime number)
+#     end --> end the if statement
+#     end --> end the iteration
+#     true --> return true if hadn't already returned false
+#   end --> end original if statement
+# end --> end method
+
+def prime?(integer) 
+  if integer <= 1 
+    false 
+  else
+    (2..integer-1).to_a.all? do |num|
+      integer % num != 0 #return true if this statement returns true for all elements in the array
+    end
+  end
+end
